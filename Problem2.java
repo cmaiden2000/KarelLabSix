2f3d7b1fb4031c81a610934064a1d5bbb33ad175
cmaiden2000 <christophem750@gmail.com>
2017-10-23 10:56:58 -0700

    g

--- Problem2.java
+++ Problem2.java
@@ -12,8 +12,70 @@ public class Problem2 extends Robot
     }
     
     public void buildFence() {
+        faceBeepers();
         
     }
-   
+    public boolean faceBeepers(){
+        // Will make Karel face North to find the Beepers 
+        if (frontIsClear()) {
+            if (facingNorth()) {
+                collectBeepers();
+            }
+            else if (facingEast()) {
+                turnLeft();
+                collectBeepers();
+            }
+        }
+        else if (facingWest()){
+            turnRight();
+            collectBeepers();
+        }
+        else {
+            turnLeft();
+            turnLeft();
+        }
+        return true;
+    }
+    public boolean collectBeepers() {
+        while (frontIsClear()) {
+            if (nextToABeeper()) {
+                while (nextToABeeper()) {
+                    pickBeeper();
+                }
+                move();
+                turnRight();
+                while (frontIsClear()) {
+                    move();
+                }
+                turnRight();
+                assembleFence();
+            }
+            else {
+                move();
+            }
+        }
+        return true;
+    }
+    public void turnRight(){
+        turnLeft();
+        turnLeft();
+        turnLeft();
+    }
+    public boolean assembleFence(){
+        if (frontIsClear()) {
+            turnLeft();
+            if (!frontIsClear()) {
+                putBeeper();
+                turnRight();
+                move();
+            }
+            else if (frontIsClear()) {
+                putBeeper();
+                move();
+            }
+            assembleFence();
+        }
+        return true;
+    }
 }
 
